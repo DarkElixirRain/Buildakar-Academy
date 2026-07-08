be8f949629a176f9f06ace86b1ec7dd58078e598
SujibShrestha <sujeeb98@gmail.com>
2026-07-08 15:10:24 +0545

    Implemented Live Class model

---
+++ backend/prisma/migrations/20260708092446_remove_recordingurl/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `recordingUrl` on the `live_classes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "live_classes" DROP COLUMN "recordingUrl";
